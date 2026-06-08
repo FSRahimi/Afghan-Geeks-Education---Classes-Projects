ebdfddd5ef15cd10995d93d1c6e7a13019a02594
Fatema Rahimi <fatemarahimi996@gmail.com>
2026-06-08 18:41:02 +0430

    don't need "" and add all in BrowserRouter

--- React-Projects/StateFormRouter-Reactjs/Product-Store-Dashboard/src/App.jsx
+++ React-Projects/StateFormRouter-Reactjs/Product-Store-Dashboard/src/App.jsx
@@ -1,10 +1,11 @@
 
 import {
+   BrowserRouter,
   Routes,
   Link,
   Route,
- BrowserRouter
 } from "react-router-dom";
+
 import Home from "./pages/Home"
 import Products from "./pages/Products"
 import ProductDetails from "./pages/ProductDetails"
@@ -14,25 +15,23 @@ import './App.css'
 function App() {
 
   return (
-  <div>
   <BrowserRouter>
         <nav>
           <Link to="/"> Home </Link>
            <Link to="/products"> Products </Link>
             <Link to="/register"> Register </Link>
-
         </nav>
-       </BrowserRouter>
+      
 
        <div>
           <Routes>
-                 <Route path="/" element="{<Home/>}"/>
-           <Route path="/products" element="{<Products/>}"/>
-              <Route path="/products/:id" element="{<ProductDetails/>}"/>
-               <Route path="/register" element="{<Register/>}"/>    
+                 <Route path="/" element={<Home/>}/>
+           <Route path="/products" element={<Products/>}/>
+              <Route path="/products/:id" element={<ProductDetails/>}/>
+               <Route path="/register" element={<Register/>}/>    
           </Routes>
        </div>
-       </div>
+        </BrowserRouter>
 )
 }
   
